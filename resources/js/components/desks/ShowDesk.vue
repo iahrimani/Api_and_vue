64bf892bfbc423eeb079f78b491ff99a904b1a4f
d.bobrikov <ahriman@inbox.ru>
2021-11-03 11:02:53 +0300

    Сделано API, возможность отображать доски, а также редактировать их имена.

--- resources/js/components/desks/ShowDesk.vue
+++ resources/js/components/desks/ShowDesk.vue
@@ -19,10 +19,23 @@ export default {
     ],
     data() {
         return {
-            desk: [],
+            desk: null,
             errored: false,
             loading: true,
         }
+    },
+    mounted() {
+        axios.get('/api/V1/desks/' + this.deskId)
+            .then(response => {
+                this.desk = response.data.data
+            })
+            .catch(error => {
+                console.log(error)
+                this.errored = true
+            })
+            .finally(() => {
+                this.loading = false
+            })
     },
     methods:{
         // https://www.youtube.com/watch?v=VwQr6LLua_M&list=PLze7bMjv1CYtP6FSv6L60IpI3rynxAStr&index=9
@@ -43,19 +56,7 @@ export default {
                 })
         }
     },
-    mounted() {
-        axios.get('/api/V1/desks/' + this.deskId)
-            .then(response => {
-                this.desk = response.data.data
-            })
-            .catch(error => {
-                console.log(error)
-                this.errored = true
-            })
-            .finally(() => {
-                this.loading = false
-            })
-    }
+
 }
 </script>
 
